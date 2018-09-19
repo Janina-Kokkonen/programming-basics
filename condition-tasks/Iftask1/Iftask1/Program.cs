07ab1fc84d39034d3375979c3d1d9a407b23eef3
Janina-Kokkonen <janina.kokkonen@student.saimia.fi>
2018-09-19 09:17:34 +0300

    condition-tasks
    folder defined

---
+++ condition-tasks/Iftask1/Iftask1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Iftask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko annettu luku" +
+                              "positiivinen+" +
+                              "negatiivinen vai nolla.");
+            Console.WriteLine("Syötä luku")
+        }
+    }
+}
